f719ffb49aee2cb75fa0fcba65a2b0af01fd580d
Masayuki Kawasaki <nu11p0er@gmail.com>
2023-06-01 16:17:15 +0900

    Add test

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 //
 // File read
 //
+#[derive(Debug)]
 pub struct Config {
     file_path: String,
 }
@@ -18,5 +19,27 @@ impl Config {
 
     pub fn get_fp(&self) -> &str {
         &self.file_path
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn build_test() {
+        let args = vec!["0".to_string()];
+        let mut config = Config::build(&args);
+        assert_eq!(config.unwrap_err(), "not enough arguments");
+
+        let args = vec!["0".to_string(), "helloworld.bin".to_string()];
+        let mut config = Config::build(&args);
+        assert_eq!(config.unwrap().file_path, "helloworld.bin");
+    }
+
+    #[test]
+    fn get_fp_test() {
+        let args = vec!["0".to_string(), "helloworld.bin".to_string()];
+        let mut config = Config::build(&args);
+        assert_eq!(config.unwrap().get_fp(), "helloworld.bin");
     }
 }
